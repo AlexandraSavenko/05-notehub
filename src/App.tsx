a49bced5fb710cbaac5a84645783d5bed35d9642
Sasha Savenko <sashasavenko3@gmail.com>
2025-09-24 20:16:01 +0300

    pagination added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import { keepPreviousData, useQuery } from "@tanstack/react-query"
 import css from "./App.module.css"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { fetchNotes } from "./services/noteService"
 import NoteList from "./components/NoteList/NoteList"
+import Pagination from "./components/Pagination/Pagination"
 function App() {
-const [searchQuery, setSearchQuery] = useState("personal")
+const [searchQuery, setSearchQuery] = useState("")
+const [page, setPage] = useState(1)
+const queryParams = {
+  searchQuery,
+  page
+}
   const {data, isFetching} = useQuery({
-    queryKey: ['notes', searchQuery],
-    queryFn: () => fetchNotes(searchQuery),
+    queryKey: ['notes', searchQuery, page],
+    queryFn: () => fetchNotes(queryParams),
     placeholderData: keepPreviousData
   })
+useEffect(() => {console.log(page)}, [page])
 const notes = data?.notes ?? [] 
+const totalPages = data?.totalPages ?? 1
+
   return (
     <div className={css.app}>
 	<header className={css.toolbar}>
 		{/* Компонент SearchBox */}
-		{/* Пагінація */}
 		{/* Кнопка створення нотатки */}
-    <NoteList notes={notes} loading={isFetching}/>
+    <Pagination itemsPerPage={notes.length} totalPages={totalPages} setPage={setPage}/>
   </header>
+  <NoteList notes={notes} loading={isFetching}/>
 </div>
   )
 }
--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -2,14 +2,19 @@ import api from "../api/api"
 import type { Note } from "../types/note"
 
 interface FetchNotesResponse {
-    notes: Note[]
+    notes: Note[],
+    totalPages: number
 }
-export const fetchNotes = async (search: string) => {
+interface FetchNotesParams {
+    searchQuery: string,
+    page: number
+}
+export const fetchNotes = async ({searchQuery, page}: FetchNotesParams): Promise<FetchNotesResponse> => {
     const searchParams: Record<string, string> = {}
-    if(search) searchParams.search = search
+    if(searchQuery) searchParams.search = searchQuery
+    if(page) searchParams.page = page.toString()
     const query = new URLSearchParams(searchParams)
 const res = await api.get<FetchNotesResponse>(`/notes?${query}`);
-console.log(res.data)
 return res.data
 }
 
